fb07294ffbf759a38b80937bfec95cf7400f461e
dgf <deep@dagnu.de>
2024-12-13 23:42:13 +0000

    2024 Day 13: describe equations solving and use destruct shorthands (Ruby)

--- 2024/13/202413.rb
+++ 2024/13/202413.rb
@@ -1,23 +1,40 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: false
 
-machines = File.read(ARGV.shift || 'example.txt')
-               .split(/\n\n/)
-               .map { |m| m.lines.map { |l| l.scan(/\d+/).map(&:to_i) } }
+#  I. ax * c + bx * d = px
+# II. ay * c + by * d = py
 
-def claw(off, machine)
-  a, b, p = machine
-  ax, ay = a
-  bx, by = b
-  px, py = p
+def calc_c(((ax, ay), (bx, by), (px, py)))
+  (px * by - py * bx) / (ax * by - ay * bx).to_f
+end
 
-  c = ((px + off) * by - (py + off) * bx) / (ax * by - ay * bx).to_f
+def calc_d(steps, ((ax, _), (bx, _), (px, _)))
+  (px - steps * ax) / bx.to_f
+end
+
+def claw(machine)
+  c = calc_c(machine)
   return 0 unless (c % 1).zero?
 
-  d = ((px + off) - c * ax) / bx.to_f
+  d = calc_d(c, machine)
   return 0 unless (d % 1).zero?
 
   c.to_i * 3 + d.to_i
 end
 
-puts "part1: #{machines.map { |m| claw(0, m) }.sum}"
-puts "part2: #{machines.map { |m| claw(10_000_000_000_000, m) }.sum}"
+machines = File.read(ARGV.shift || 'example.txt')
+               .split(/\n\n/)
+               .map { |m| m.lines.map { |l| l.scan(/\d+/).map(&:to_i) } }
+
+puts "part1: #{machines
+  .map(&method(:claw))
+  .sum}"
+
+def adjust(offset)
+  ->((a, b, (px, py))) { [a, b, [px + offset, py + offset]] }
+end
+
+puts "part2: #{machines
+  .map(&adjust(10_000_000_000_000))
+  .map(&method(:claw))
+  .sum}"
